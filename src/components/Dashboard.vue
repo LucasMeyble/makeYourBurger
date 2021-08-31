8f1ae759a3bdda6f673f1343e9ba71a3222f49bc
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-31 14:33:06 -0300

    Resgatando pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,16 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Joao</div>
-                <div>Pão de trigo</div>
-                <div>maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{burger.id}}</div>
+                <div>{{burger.nome}}</div>
+                <div>{{burger.pao}}</div>
+                <div>{{burger.carne}}</div>
                 <div>
                     <ul>
-                        <li>salame</li>
-                        <li>tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{opcional}}
+                        </li>
                     </ul>
                 </div>
                 <div>
@@ -35,7 +36,29 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+        return{
+            burger: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos() {
+            const req = await fetch("http://localhost:3000/burgers")
+
+            const data = await req.json();
+            this.burgers = data;
+            
+            console.log(this.burgers);
+            //resgatar os status
+        }
+    },
+
+    mounted() {
+        this.getPedidos(); 
+    }
 }
 </script>
 
